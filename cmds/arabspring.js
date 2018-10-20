8d1f1f0ceace83e7b418f07964dc6125e207b61a
metroQ <jakeblizzard513@gmail.com>
2018-10-20 09:42:14 -0400

    Testing functionality of arabspring

--- cmds/arabspring.js
+++ cmds/arabspring.js
@@ -1,13 +1,13 @@
 module.exports.run = async (bot, message, args) => {
     let target = message.mentions.users.first() || message.author;
-    let chars = message.split();
-    let imageNumber = chars[12];
+    let arr = message.split(" ");
+    let num = arr[1];
 
     let embededImage = new Discord.RichEmbed()
-        .setImage("/images/%s", imageNumber) //get corresponding image from directory
+        .setImage("/arabDirectory/%s", num) //get corresponding image from directory
     
     message.channel.send({embededImage: embededImage}) //send it yo
 }
 module.exports.help = {
-    name: "randomimag"
+    name: "arabspring"
 }	
