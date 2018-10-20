19912b1c225ab50d2b6e4181930051f045c22079
sizzurp34 <sparklefiners@gmail.com>
2018-10-19 22:33:57 -0400

    Removed Faggotry

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,5 +1,4 @@
 const Discord = module.require("discord.js");
-//yunus ur cute love urself
 module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
         .setAuthor(message.author.username)
