0da45083b88f9652957c569f0d7954f090076e61
metroQ <jakeblizzard513@gmail.com>
2018-10-20 10:01:26 -0400

    Update cmds/arabspring.js

--- cmds/arabspring.js
+++ cmds/arabspring.js
@@ -1,10 +1,11 @@
 module.exports.run = async (bot, message, args) => {
     let target = message.mentions.users.first() || message.author;
-    let arr = message.split(" ");
+    let newMes = message.toString();
+    let arr = newMes.split(" ");
     let num = arr[1];
 
     let embededImage = new Discord.RichEmbed()
-        .setImage("/arabDirectory/%s", num) //get corresponding image from directory
+        .setImage("./arabDirectory/%s", num) //get corresponding image from directory
     
     message.channel.send({embededImage: embededImage}) //send it yo
 }
