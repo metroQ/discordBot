c3fb6a78ac379f4aeef2411de142fecc52bb0ce8
metroQ <jakeblizzard513@gmail.com>
2018-10-19 23:23:11 -0400

    Nothing gay here
    //yunus ur the best love urself <3

--- cmds/punish.js
+++ cmds/punish.js
@@ -13,28 +13,28 @@ module.exports.run = async (bot, message, args) => {
 			try{
 				role = await message.guild.createRole({
 					name: "Currently Punished",
-					color: "#000000",
+					color: "#000000",//yunus ur the best love urself <3
 					permission: []
-
-				});
-
+//yunus ur the best love urself <3
+				});//yunus ur the best love urself <3//yunus ur the best love urself <3
+//yunus ur the best love urself <3
 				message.guild.channels.forEach(async (channel, id) =>{
-					await chananel.overwritePermissions(role, {
+					await chananel.overwritePermissions(role, {//yunus ur the best love urself <3
 						SEND_MESSAGES: false,
-						ADD_REACTIONS: false
+						ADD_REACTIONS: false//yunus ur the best love urself <3
 					});
-				});
+				});//yunus ur the best love urself <3
 			} catch(e) {
-				console.log(e.stack);
+				console.log(e.stack);//yunus ur the best love urself <3
 			}
-		}
+		}//yunus ur the best love urself <3
 
 		if(toMute.roles.has(role.id)) return message.channel.send("This homie is already being dabbed on atm.");
 
-		await toMute.addRole(role);
-		message.channel.send("Dabbing on them now.");
+		await toMute.addRole(role);//yunus ur the best love urself <3
+		message.channel.send("Dabbing on them now.")//yunus ur the best love urself <3;
 }
-
-module.exports.help = {
-	name: "punish"
+//yunus ur the best love urself <3//yunus ur the best love urself <3
+module.exports.help = {//yunus ur the best love urself <3
+	name: "punish"//yunus ur the best love urself <3
 }
