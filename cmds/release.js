c0345ac9ff88bc35e1593dab1ddaff666a2f1ee5
metroQ <jakeblizzard513@gmail.com>
2018-10-19 23:05:07 -0400

    release(un-punish) commnd
    dab

---
+++ cmds/release.js
@@ -0,0 +1,18 @@
+module.exports.run =async (bot, message, args) => {
+	if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You do not have permission to release the target.");
+
+	let toMute = message.mentions.members.first() || message.guild.members.get(args[0]);
+	if(!toMute) return message.channel.send("You didn't specify a target.");
+
+	let role = message.guild.roles.find(r => r.name == "Currently Punished");
+
+	if(!role || !toMute.roles.has(role.id)) return message.channel.send("This user is not being punished!");
+
+	await toMute.removeRole(role);
+	message.channel.send("They have been released")
+
+}
+
+module.exports.help = {
+	name: "releases"
+}
