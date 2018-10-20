2a563b2f17a4db8849e83664b81f6752d631095d
sizzurp34 <sparklefiners@gmail.com>
2018-10-19 21:27:41 -0400

    Changed TOKEN

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require(`discord.js`);
 const bot = new Discord.Client();
-const TOKEN = `1LEfSQzXEkVnzRDmykz-A-OEXefBm2Bo`
+const TOKEN = `NTAzMDE0NjkwOTYwNDQxMzc0.DqwV6Q.b76VTKYgy7cbYoh_JfhLu2gxf9Q`
 
 bot.login(TOKEN);
