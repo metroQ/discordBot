ed261a9b7d856cc3db7d60025f2ba48fe6ae7f53
sizzurp34 <sparklefiners@gmail.com>
2018-10-19 22:31:59 -0400

    Command Manager
    -Added Command Manager

---
+++ cmds/userinfo.js
@@ -0,0 +1,17 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let embed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setDescription("User info")
+        .setColor("#FFFFFF")
+        .addField("Full Username", message.author.tag)
+        .addField("ID", message.author.id)
+        .addField("Created At", message.author.createdAt)
+    
+    message.channel.send({embed: embed})
+}
+
+module.exports.help = {
+    name: "userinfo"
+}
--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const botSettings = require("./botsettings.json")
 const Discord = require(`discord.js`);
+const fs = require("fs");
+
+const prefix = botSettings.prefix;
+
 const bot = new Discord.Client();
 
-bot.on("ready", () =>{
-    console.log(`${bot.user.username} up and running...`)
+bot.commands = new Discord.Collection();
+fs.readdir("./cmds/", (err, files) => {
+    if(err) console.error(err);
+
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <= 0) {
+        console.log("No commands to load...");
+        return;
+    }
+
+    console.log(`Loading ${jsfiles.length} commands...`)
+
+    jsfiles.forEach((f, i) => {
+        let props = require(`./cmds/${f}`);
+        console.log(`${i + 1}: ${f} loaded...`);
+        bot.commands.set(props.help.name, props);
+    });
 });
+
+bot.on("ready", () =>{
+    console.log(`${bot.user.username} up and running...`);
+    console.log(bot.commands);
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(/\s+/g);
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return;
+
+    let cmd = bot.commands.get(command.slice(prefix.length));
+    if(cmd) cmd.run(bot, message, args);
+});
+
 bot.login(botSettings.token);
